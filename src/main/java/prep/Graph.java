b71b584e4fe97e2b69d401b2bcafc501f54e6f3d
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-11-29 15:15:47 +0000

    implement fundamental methods

--- src/main/java/prep/Graph.java
+++ src/main/java/prep/Graph.java
@@ -1,4 +1,27 @@
 package prep;
 
+import org.apache.jena.rdf.model.*;
+import org.apache.jena.util.FileManager;
+
+import java.io.InputStream;
+
 public class Graph {
+    private final Model model;
+    private final String resourceURIStart = "http://nlp/resources/synsets/WordNetNounSynset#";
+
+    public Graph(){
+        this.model = ModelFactory.createDefaultModel();
+    }
+
+    public void addModel(String graphLocation){
+        InputStream graphFile = FileManager.get().open(graphLocation);
+        if (graphFile == null){
+            throw new IllegalArgumentException("File: " + graphLocation + " not found");
+        }
+        this.model.read(graphLocation);
+    }
+
+    public Definendum findDefindendum(String definendumName){
+        return new Definendum(this.model.getResource(this.resourceURIStart + definendumName))
+    }
 }
