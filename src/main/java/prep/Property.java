4799c97652126d1eea57847981927095508d957b
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-11-29 18:46:28 +0000

    implement core methods for property

--- src/main/java/prep/Property.java
+++ src/main/java/prep/Property.java
@@ -1,4 +1,26 @@
 package prep;
 
+
+import org.apache.jena.rdf.model.Statement;
+import org.apache.jena.rdf.model.impl.LiteralImpl;
+import org.apache.jena.vocabulary.RDF;
+
 public class Property {
+    private final Statement statement;
+
+    public Property(Statement statement){
+        this.statement = statement;
+    }
+
+    public String toString(){
+        if(this.statement.getObject() instanceof LiteralImpl){
+            return this.statement.getProperty(RDF.object).getObject().toString();
+        }else{
+            return this.statement.getProperty(RDF.object).getProperty(RDF.object).getObject().toString();
+        }
+    }
+
+    public String getRole(){
+        return this.statement.getProperty(RDF.predicate).getResource().getLocalName();
+    }
 }
