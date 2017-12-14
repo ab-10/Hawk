1a2d32056b6fee7e12c79f41219381b8eaf59d2f
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-12-14 11:11:59 +0000

    implement the new Property design

--- src/main/java/prep/Property.java
+++ src/main/java/prep/Property.java
@@ -1,18 +1,27 @@
 package prep;
 
 public class Property {
-    private final String value, role;
+    private final String value, role, subject;
 
-    public Property(String value, String role){
+    public Property(String value, String role, String subject){
         this.value = value;
         this.role = role;
+        this.subject = subject;
     }
 
     public String toString(){
+        return "Property(" + value + ", " + role + ", " + subject + ")";
+    }
+
+    public String getValue(){
         return this.value;
     }
 
     public String getRole(){
         return this.role;
+    }
+
+    public String getSubject(){
+        return this.subject;
     }
 }
