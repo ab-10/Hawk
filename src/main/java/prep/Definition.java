6fea9014c6a5c2e2dca7c05cd72e474e7e3de663
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-12-14 00:48:25 +0000

    implement the new Definition design

--- src/main/java/prep/Definition.java
+++ src/main/java/prep/Definition.java
@@ -7,42 +7,43 @@ import org.apache.jena.vocabulary.RDF;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.ListIterator;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class Definition {
-    private final Resource resource;
+    private final String URI;
+    private final String value;
+    private List<Property> properties;
 
-    public Definition(Resource resource){
-        this.resource = resource;
+    public Definition(String URI){
+        this.URI = URI;
+        this.value = generateValue(this.URI);
+        this.properties = new ArrayList<>();
     }
 
-    public List<Property> listProperties(){
-        ArrayList<Property> properties = new ArrayList<>();
-        StmtIterator propertyIterator = this.resource.listProperties(RDF.type);
+    public void addProperty(Property propertyToAdd){
+        properties.add(propertyToAdd);
+    }
 
-        while(propertyIterator.hasNext()){
-            String value;
-            String role;
-            Statement currentStatement = propertyIterator.next();
-
-            // if currentStatement is nested it selects the innermost role and value
-            // TODO: the model should be improved to also parse the outer roles and values
-            // TODO: role should be set to the local role value (as opposed to global)
-            if(currentStatement.getProperty(RDF.object).getObject().isLiteral()){
-                value = currentStatement.getProperty(RDF.object).getObject().toString();
-                role = currentStatement.getProperty(RDF.predicate).getObject().toString();
-            }else{
-                value = currentStatement.getProperty(RDF.object).getProperty(RDF.object).getObject().toString();
-                role = currentStatement.getProperty(RDF.object).getProperty(RDF.predicate).getObject().toString();
-            }
-
-            properties.add(new Property(value, role));
-        }
-
-        return properties;
+    public void addProperty(List<Property> propertiesToAdd){
+        properties.addAll(propertiesToAdd);
     }
 
     public String toString(){
-        return this.resource.getLocalName();
+        String result = "Definition(" + value + ", ";
+        for(int i = 0; i < (properties.size() - 1); i++){
+            result += properties.get(i) + ", ";
+        }
+        result += properties.get(properties.size() - 1) + ")";
+        return result;
     }
 
+    // Extracts the local name from the URI, for use as the Definition's value
+    private static String generateValue(String URI){
+        Pattern regExPattern = Pattern.compile("(?<=#).*(?=\\>)");
+        Matcher matcherForURI = regExPattern.matcher(URI);
+        matcherForURI.find();
+        return matcherForURI.group();
+    }
 }
