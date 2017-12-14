1dfa9bd4cca9c61ddcbaa9565a4924f3195d2cf5
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-12-14 12:55:09 +0000

    implement helper methods to accomodate the new Graph design

--- src/main/java/prep/Graph.java
+++ src/main/java/prep/Graph.java
@@ -1,25 +1,80 @@
 package prep;
 
+import org.apache.jena.query.*;
 import org.apache.jena.rdf.model.*;
 
 import java.io.InputStream;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Graph {
     private final Model model;
+    private List<Definition> definitions;
     private final String resourceURIStart = "http://nlp/resources/synsets/WordNetNounSynset#";
 
-    public Graph(){
-        this.model = ModelFactory.createDefaultModel();
+    public Graph(String graphLocation){
+        model = createPopulatedModel(graphLocation);
+        definitions = findAllDefinitions();
+        populateAllDefinitions(definitions, model);
     }
 
-    public void addModel(String graphLocation){
+    private static Model createPopulatedModel(String modelLocation){
+        Model modelCreated = ModelFactory.createDefaultModel();
+
         ClassLoader loader = Thread.currentThread().getContextClassLoader();
-        InputStream graphFile = loader.getResourceAsStream(graphLocation);
+        InputStream graphFile = loader.getResourceAsStream(modelLocation);
         // InputStream graphFile = FileManager.get().open(graphLocation);
         if (graphFile == null){
-            throw new IllegalArgumentException("File: " + graphLocation + " not found");
+            throw new IllegalArgumentException("File: " + modelLocation + " not found");
         }
-        this.model.read(graphLocation);
+        modelCreated.read(modelLocation);
+        return modelCreated;
+    }
+
+    private static List<Definition> findAllDefinitions(Model model){
+        List<Definition> definitions = new ArrayList<>();
+        String queryString = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+                "SELECT DISTINCT ?definition " +
+                "WHERE{" +
+                "?definition rdf:type ?property ." +
+                "FILTER(?property != rdf:Statement)" +
+                "}";
+
+        Query SPARQLquery = QueryFactory.create(queryString);
+        try(QueryExecution qexec = QueryExecutionFactory.create(SPARQLquery, model)){
+            ResultSet results = qexec.execSelect();
+            while(results.hasNext()){
+                QuerySolution solution = results.nextSolution();
+                Resource currentDefinition = solution.getResource("definition");
+                definitions.add(new Definition(currentDefinition.getURI()));
+            }
+        }
+        return definitions;
+    }
+
+    private static void populateAllDefinitions(List<Definition> definitions, Model model){
+        String queryTemplate = "PREFIX rdf: <http://www.w3.org/1999/02/22-rdf-syntax-ns#> " +
+                "SELECT ?value ?role ?subject" +
+                "WHERE{" +
+                "%s        rdf:type      ?property ." +
+                "?property rdf:object    ?value ." +
+                "?property rdf:predicate ?role ." +
+                "?property rdf:subject   ?subject" +
+                "}";
+        for(Definition currentDefinition : definitions){
+            String currentQueryString = String.format(queryTemplate, currentDefinition.getURI());
+            Query currentQuery = QueryFactory.create(currentQueryString);
+            try(QueryExecution qexec = QueryExecutionFactory.create(currentQuery, model)){
+                ResultSet properties = qexec.execSelect();
+                while(properties.hasNext()){
+                    QuerySolution currentProperty = properties.nextSolution();
+                    String currentValue = currentProperty.getResource("value").getLocalName();
+                    String currentRole = currentProperty.getResource("role").getLocalName();
+                    String currentSubject = currentProperty.getResource("subject").getLocalName();
+                    currentDefinition.addProperty(new Property(currentValue, currentRole, currentSubject));
+                }
+            }
+        }
     }
 
     public Definition findDefinition(String definendumName){
