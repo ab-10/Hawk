a68c216bf0e24621acb514fd30bd8530ef147d78
Jean-Paul Sartre <armins.bagrats@gmail.com>
2017-12-14 17:10:06 +0000

    create getProperties()

--- src/main/java/prep/Definition.java
+++ src/main/java/prep/Definition.java
@@ -1,24 +1,18 @@
 package prep;
 
-import org.apache.jena.rdf.model.Resource;
-import org.apache.jena.rdf.model.Statement;
-import org.apache.jena.rdf.model.StmtIterator;
-import org.apache.jena.vocabulary.RDF;
-
 import java.util.ArrayList;
 import java.util.List;
-import java.util.ListIterator;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Definition {
     private final String URI;
-    private final String value;
+    private final String definiendum;
     private List<Property> properties;
 
     public Definition(String URI){
         this.URI = URI;
-        this.value = generateValue(this.URI);
+        this.definiendum = generateValue(this.URI);
         this.properties = new ArrayList<>();
     }
 
@@ -34,8 +28,17 @@ public class Definition {
         properties.addAll(propertiesToAdd);
     }
 
+    public List<Property> getProperties() {
+        return properties;
+    }
+
+    public String getDefiniendum() {
+
+        return definiendum;
+    }
+
     public String toString(){
-        String result = "Definition(" + value + ", ";
+        String result = "Definition(" + definiendum + ", ";
         for(int i = 0; i < (properties.size() - 1); i++){
             result += properties.get(i) + ", ";
         }
@@ -43,7 +46,7 @@ public class Definition {
         return result;
     }
 
-    // Extracts the local name from the URI, for use as the Definition's value
+    // Extracts the local name from the URI, for use as the Definition's definiendum
     private static String generateValue(String URI){
         Pattern regExPattern = Pattern.compile("(?<=#).*(?=\\>)");
         Matcher matcherForURI = regExPattern.matcher(URI);
