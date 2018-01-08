eec8617676a41fc35eba30bbf7211595321dd7fa
Jean-Paul Sartre <armins.bagrats@gmail.com>
2018-01-08 20:04:14 +0200

    switch to CharTokenizer

--- src/main/java/indexation/DefinitionAnalyzer.java
+++ src/main/java/indexation/DefinitionAnalyzer.java
@@ -17,107 +17,119 @@
 package indexation;
 
 
+import org.apache.lucene.analysis.*;
+import org.apache.lucene.analysis.standard.StandardTokenizer;
+import org.apache.lucene.analysis.util.CharTokenizer;
+
 import java.io.IOException;
 import java.io.Reader;
 import java.util.Arrays;
 import java.util.List;
-
-import org.apache.lucene.analysis.CharArraySet;
-import org.apache.lucene.analysis.LowerCaseFilter;
-import org.apache.lucene.analysis.StopFilter;
-import org.apache.lucene.analysis.StopwordAnalyzerBase;
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.WordlistLoader;
 
 /**
  * A modification of Lucene's Standard Analyzer.
  */
 public final class DefinitionAnalyzer extends StopwordAnalyzerBase {
 
-  /** An unmodifiable set containing some common English words that are not usually useful
-  for searching.*/
-  public static final CharArraySet ENGLISH_STOP_WORDS_SET;
-  
-  static {
-    final List<String> stopWords = Arrays.asList(
-      "a", "an", "and", "are", "as", "at", "be", "but", "by",
-      "for", "if", "in", "into", "is", "it",
-      "no", "not", "of", "on", "or", "such",
-      "that", "the", "their", "then", "there", "these",
-      "they", "this", "to", "was", "will", "with"
-    );
-    final CharArraySet stopSet = new CharArraySet(stopWords, false);
-    ENGLISH_STOP_WORDS_SET = CharArraySet.unmodifiableSet(stopSet); 
-  }
-  
-  /** Default maximum allowed token length */
-  public static final int DEFAULT_MAX_TOKEN_LENGTH = 255;
+    /**
+     * An unmodifiable set containing some common English words that are not usually useful
+     * for searching.
+     */
+    public static final CharArraySet ENGLISH_STOP_WORDS_SET;
 
-  private int maxTokenLength = DEFAULT_MAX_TOKEN_LENGTH;
+    static {
+        final List<String> stopWords = Arrays.asList(
+                "a", "an", "and", "are", "as", "at", "be", "but", "by",
+                "for", "if", "in", "into", "is", "it",
+                "no", "not", "of", "on", "or", "such",
+                "that", "the", "their", "then", "there", "these",
+                "they", "this", "to", "was", "will", "with"
+        );
+        final CharArraySet stopSet = new CharArraySet(stopWords, false);
+        ENGLISH_STOP_WORDS_SET = CharArraySet.unmodifiableSet(stopSet);
+    }
 
-  /** An unmodifiable set containing some common English words that are usually not
-  useful for searching. */
-  public static final CharArraySet STOP_WORDS_SET = ENGLISH_STOP_WORDS_SET;
+    /**
+     * Default maximum allowed token length
+     */
+    public static final int DEFAULT_MAX_TOKEN_LENGTH = 255;
 
-  /** Builds an analyzer with the given stop words.
-   * @param stopWords stop words */
-  public DefinitionAnalyzer(CharArraySet stopWords) {
-    super(stopWords);
-  }
+    private int maxTokenLength = DEFAULT_MAX_TOKEN_LENGTH;
 
-  /** Builds an analyzer with the default stop words ({@link #STOP_WORDS_SET}).
-   */
-  public DefinitionAnalyzer() {
-    this(STOP_WORDS_SET);
-  }
+    /**
+     * An unmodifiable set containing some common English words that are usually not
+     * useful for searching.
+     */
+    public static final CharArraySet STOP_WORDS_SET = ENGLISH_STOP_WORDS_SET;
 
-  /** Builds an analyzer with the stop words from the given reader.
-   * @see WordlistLoader#getWordSet(Reader)
-   * @param stopwords Reader to read stop words from */
-  public DefinitionAnalyzer(Reader stopwords) throws IOException {
-    this(loadStopwordSet(stopwords));
-  }
+    /**
+     * Builds an analyzer with the given stop words.
+     *
+     * @param stopWords stop words
+     */
+    public DefinitionAnalyzer(CharArraySet stopWords) {
+        super(stopWords);
+    }
 
-  /**
-   * Set the max allowed token length.  Tokens larger than this will be chopped
-   * up at this token length and emitted as multiple tokens.  If you need to
-   * skip such large tokens, you could increase this max length, and then
-   * use {@code LengthFilter} to remove long tokens.  The default is
-   * {@link DefinitionAnalyzer#DEFAULT_MAX_TOKEN_LENGTH}.
-   */
-  public void setMaxTokenLength(int length) {
-    maxTokenLength = length;
-  }
-    
-  /** Returns the current maximum token length
-   * 
-   *  @see #setMaxTokenLength */
-  public int getMaxTokenLength() {
-    return maxTokenLength;
-  }
+    /**
+     * Builds an analyzer with the default stop words ({@link #STOP_WORDS_SET}).
+     */
+    public DefinitionAnalyzer() {
+        this(STOP_WORDS_SET);
+    }
 
-  @Override
-  protected TokenStreamComponents createComponents(final String fieldName) {
-    final DefinitionTokenizer src = new DefinitionTokenizer();
-    src.setMaxTokenLength(maxTokenLength);
-    TokenStream tok = new EmptyTokenFilter(src);
-    tok = new LowerCaseFilter(tok);
-    tok = new StopFilter(tok, stopwords);
-    return new TokenStreamComponents(src, tok) {
-      @Override
-      protected void setReader(final Reader reader) {
-        // So that if maxTokenLength was changed, the change takes
-        // effect next time tokenStream is called:
-        src.setMaxTokenLength(DefinitionAnalyzer.this.maxTokenLength);
-        super.setReader(reader);
-      }
-    };
-  }
+    /**
+     * Builds an analyzer with the stop words from the given reader.
+     *
+     * @param stopwords Reader to read stop words from
+     * @see WordlistLoader#getWordSet(Reader)
+     */
+    public DefinitionAnalyzer(Reader stopwords) throws IOException {
+        this(loadStopwordSet(stopwords));
+    }
 
-  @Override
-  protected TokenStream normalize(String fieldName, TokenStream in) {
-    TokenStream result = new EmptyTokenFilter(in);
-    result = new LowerCaseFilter(result);
-    return result;
-  }
+    /**
+     * Set the max allowed token length.  Tokens larger than this will be chopped
+     * up at this token length and emitted as multiple tokens.  If you need to
+     * skip such large tokens, you could increase this max length, and then
+     * use {@code LengthFilter} to remove long tokens.  The default is
+     * {@link DefinitionAnalyzer#DEFAULT_MAX_TOKEN_LENGTH}.
+     */
+    public void setMaxTokenLength(int length) {
+        maxTokenLength = length;
+    }
+
+    /**
+     * Returns the current maximum token length
+     *
+     * @see #setMaxTokenLength
+     */
+    public int getMaxTokenLength() {
+        return maxTokenLength;
+    }
+    @Override
+    protected TokenStreamComponents createComponents(final String fieldName) {
+        // final DefinitionTokenizer src = new DefinitionTokenizer();
+        Tokenizer src = CharTokenizer.fromSeparatorCharPredicate(ch -> Character.isWhitespace(ch) || ch == '_');
+        // src.setMaxTokenLength(maxTokenLength);
+        TokenStream tok = new EmptyTokenFilter(src);
+        tok = new LowerCaseFilter(tok);
+        tok = new StopFilter(tok, stopwords);
+        return new TokenStreamComponents(src, tok);
+        /* {
+            @Override
+            protected void setReader(final Reader reader)
+                // So that if maxTokenLength was changed, the change takes
+                // effect next time tokenStream is called:
+                src.setMaxTokenLength(DefinitionAnalyzer.this.maxTokenLength);
+                super.setReader(reader);
+            }*/
+    }
+
+    @Override
+    protected TokenStream normalize(String fieldName, TokenStream in) {
+        TokenStream result = new EmptyTokenFilter(in);
+        result = new LowerCaseFilter(result);
+        return result;
+    }
 }
