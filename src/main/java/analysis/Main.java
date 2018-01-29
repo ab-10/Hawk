db9ba67be101f45dacbe991dc6b14973acc79fe8
Jean-Paul Sartre <armins.bagrats@gmail.com>
2018-01-29 02:50:24 +0000

    make a sample knn request

--- src/main/java/analysis/Main.java
+++ src/main/java/analysis/Main.java
@@ -1,5 +1,14 @@
 package analysis;
 
+import org.apache.commons.io.IOUtils;
+import org.apache.http.*;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.entity.UrlEncodedFormEntity;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.ByteArrayEntity;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.HttpClients;
+import org.apache.http.message.BasicNameValuePair;
 import org.apache.lucene.index.*;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.Directory;
@@ -7,7 +16,11 @@ import org.apache.lucene.store.FSDirectory;
 import prep.Graph;
 
 import java.io.*;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 import static indexation.GraphIndexer.indexGraph;
@@ -15,6 +28,8 @@ import static indexation.GraphIndexer.indexGraph;
 public class Main {
     private static Directory graphDirectory;
     public static void main(String[] args) throws Exception{
+        compare("foo", "bar", "tar");
+        /*
         graphDirectory = FSDirectory.open(Paths.get("src", "main", "resources", "index"));
         Graph graph = new Graph("WN_DSR_model_XML.rdf");
         indexGraph(graph, graphDirectory);
@@ -28,6 +43,7 @@ public class Main {
 
         }
         resultWriter.close();
+        */
 
     }
 
@@ -36,6 +52,41 @@ public class Main {
         comparison = comparison.toLowerCase();
         feature = feature.toLowerCase();
 
+        HttpClient httpclient = HttpClients.createDefault();
+        HttpPost httppost = new HttpPost("http://indra.lambda3.org/neighbors/relatedness");
+        httppost.setHeader("Content-Type", "application/json");
+
+// Request parameters and other properties.
+        String params = "{\n" +
+                "        \"corpus\": \"wiki-2014\",\n" +
+                "        \"model\": \"W2V\",\n" +
+                "        \"language\": \"EN\",\n" +
+                "        \"topk\": 10,\n" +
+                "\"scoreFunction\": \"COSINE\",\n" +
+                "        \"terms\" : [\"mother\"]\n" +
+                "}";
+        httppost.setEntity(new ByteArrayEntity(params.getBytes("UTF-8")));
+
+//Execute and get the response.
+        HttpResponse response = httpclient.execute(httppost);
+        StatusLine status = response.getStatusLine();
+        HttpEntity entity = response.getEntity();
+
+        if (entity != null) {
+            InputStream instream = entity.getContent();
+            try {
+                StringWriter writer = new StringWriter();
+                IOUtils.copy(instream, writer, "UTF-8");
+                String theString = writer.toString();
+                System.out.println(theString);
+                System.out.println(status);
+            } finally {
+                instream.close();
+            }
+        }
+        return 1;
+
+        /*
         BooleanQuery.Builder builderPivot = new BooleanQuery.Builder();
         BooleanQuery.Builder builderComparison = new BooleanQuery.Builder();
         builderPivot.add(new TermQuery(new Term("definiendum", pivot)), BooleanClause.Occur.MUST);
@@ -60,6 +111,6 @@ public class Main {
         }else{
             return 0;
         }
-
+        */
     }
 }
