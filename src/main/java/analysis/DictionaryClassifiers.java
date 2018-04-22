ea3442cf5d74a7cd801107deb6b4055519b4d29e
Jean-Paul Sartre <armins.bagrats@gmail.com>
2018-04-22 17:46:56 +0100

    stub DictionaryClassifiers

--- src/main/java/analysis/DictionaryClassifiers.java
+++ src/main/java/analysis/DictionaryClassifiers.java
@@ -1,4 +1,13 @@
 package analysis;
 
 public class DictionaryClassifiers {
+    public static int wordNetVote(String pivot, String comparison, String feature, String indexLocation){
+        // stub value
+        return 0;
+    }
+
+    public static int visualGenomeVote(String pivot, String comparison, String feature, String indexLocation){
+        // stub value
+        return 0;
+    }
 }
