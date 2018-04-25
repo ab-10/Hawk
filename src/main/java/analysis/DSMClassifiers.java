fabeb12fb2adfeec7803b25f6e9e199981f0ed13
Jean-Paul Sartre <armins.bagrats@gmail.com>
2018-04-25 18:27:48 +0100

    create stubs

---
+++ src/main/java/analysis/DSMClassifiers.java
@@ -0,0 +1,21 @@
+package analysis;
+
+/** A collection of discriminativity classifiers based on Distributional Semantic Models
+ *
+ */
+public class DSMClassifiers {
+    public static int w2vVote(String pivot, String comparison, String feature){
+        // stub value
+        return 0;
+    }
+
+    public  static int lsaVote(String pivot, String comparison, String feature){
+        // stub value
+        return 0;
+    }
+
+    public static int gloVeVote(String pivot, String comparison, String feature){
+        // stub value
+        return 0;
+    }
+}
