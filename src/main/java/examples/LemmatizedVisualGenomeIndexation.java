3c600ddde33de333226af65bd9aa59a85651686a
Jean-Paul Sartre <armins.bagrats@gmail.com>
2018-05-05 19:55:41 +0100

    provide examples of updated models

--- src/main/java/examples/LemmatizedVisualGenomeIndexation.java
+++ src/main/java/examples/LemmatizedVisualGenomeIndexation.java
@@ -1,4 +1,35 @@
 package examples;
 
-public class LemmatizedVisualGenomeIndexation {
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.file.Paths;
+import java.util.Scanner;
+
+import static analysis.DictionaryClassifiers.visualGenomeVote;
+import static indexation.VisualGenomeIndexer.indexGenomeAttributes;
+
+
+public class LemmatizedVisualGenomeIndexation{
+    public static void main(String args[]) throws IOException{
+        // creates a Lucene index from lemmatized WN graph
+        String indexLocation = "src/main/resources/lemmatizedVG";
+        Directory indexDir = FSDirectory.open(Paths.get(indexLocation));
+        File source = new File("src/main/resources/attributes.json");
+        indexGenomeAttributes(source, indexDir);
+
+        // evaluates the index against sample data
+        FileWriter resultWriter = new FileWriter("src/main/resources/lemmatizedVG.results");
+        Scanner taskScanner = new Scanner(new File("src/main/resources/truth.txt"));
+        while (taskScanner.hasNext()) {
+            String[] currentLine = taskScanner.nextLine().split(",");
+            int result = visualGenomeVote(currentLine[0], currentLine[1], currentLine[2], indexLocation);
+            resultWriter.write(currentLine[0] + "," + currentLine[1] + "," + currentLine[2] + "," + result + "\n");
+        }
+        taskScanner.close();
+        resultWriter.close();
+    }
 }
--- src/main/java/examples/LemmatizedWordNetIndexation.java
+++ src/main/java/examples/LemmatizedWordNetIndexation.java
@@ -16,13 +16,13 @@ import static indexation.GraphIndexer.indexGraph;
 public class LemmatizedWordNetIndexation {
     public static void main(String args[]) throws IOException{
         // creates a Lucene index from lemmatized WN graph
-        String indexLocation = "src/main/resources/lemmatizedWN";
-        // Directory indexDir = FSDirectory.open(Paths.get(indexLocation));
-        // Graph wnGraph = new Graph("WN_DSR_model_XML.rdf");
-        // indexGraph(wnGraph, indexDir);
+        String indexLocation = "src/main/resources/lemmatizedWNNoHyp";
+        Directory indexDir = FSDirectory.open(Paths.get(indexLocation));
+        Graph wnGraph = new Graph("WN_DSR_model_XML.rdf");
+        indexGraph(wnGraph, indexDir);
 
         // evaluates the index against sample data
-        FileWriter resultWriter = new FileWriter("src/main/resources/lemmatizedWN.results");
+        FileWriter resultWriter = new FileWriter("src/main/resources/lemmatizedWNNoHyp.results");
         Scanner taskScanner = new Scanner(new File("src/main/resources/truth.txt"));
         while(taskScanner.hasNext()){
             String[] currentLine = taskScanner.nextLine().split(",");
