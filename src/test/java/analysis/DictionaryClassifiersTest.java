5c23da6456eefcac5dfa2092d7a85a2b7f0f4075
Jean-Paul Sartre <armins.bagrats@gmail.com>
2018-06-21 18:45:45 +0100

    adjust tests based on latest models

--- src/test/java/analysis/DictionaryClassifiersTest.java
+++ src/test/java/analysis/DictionaryClassifiersTest.java
@@ -11,16 +11,18 @@ class DictionaryClassifiersTest {
     @Test
     void testWordNetVote() {
         assertEquals(1, wordNetVote("cheek", "brow", "red",
-                "src/test/resources/WNWithHyp"), "WordNet with hypernym expansion should recognize cheek,brow,red as discriminative");
-        assertEquals(0, wordNetVote("neck", "throat", "wide",
-                "src/test/resources/WNWithHyp"), "WN shouldn't recognize neck,throat,wide as discriminative");
+                "src/main/resources/WNWithHyp"), "WordNet with hypernym expansion should recognize cheek,brow,red as discriminative");
+        assertEquals(1, wordNetVote("neck", "throat", "wide",
+                "src/main/resources/WNWithHyp"), "WN should recognize neck,throat,wide as discriminative");
+        assertEquals(0, wordNetVote("bus", "train", "transport",
+                "src/main/resources/WNWithHyp"), "WN shouldn't recognize bus,train,transport as discriminative");
     }
 
     @Test
     void testVisualGenomeVote() {
-        assertEquals(1, visualGenomeVote("banana", "strawberry", "yellow",
-                "src/test/resources/VG"), "Does not recognize triple banana,strawberry,yellow as discriminative");
+        assertEquals(1, visualGenomeVote("king", "prince", "crown",
+                "src/main/resources/oldIndexes/VG"), "Does not recognize triple king,prince,crown as discriminative");
         assertEquals(0, visualGenomeVote("chimp", "giraffe", "fur",
-                "src/test/resources/VG"), "Incorrectly recognizes chimp,giraffe,fur as discriminative");
+                "src/main/resources/VG"), "Incorrectly recognizes chimp,giraffe,fur as discriminative");
     }
 }
