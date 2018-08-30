37dc0b190317e9c8053d2c0dfe39f60220d2bd82
Jean-Paul Sartre <armins.bagrats@gmail.com>
2018-08-30 14:06:14 +0100

    implement role-wise classification

--- src/main/java/analysis/DictionaryClassifiers.java
+++ src/main/java/analysis/DictionaryClassifiers.java
@@ -2,18 +2,81 @@ package analysis;
 
 import net.didion.jwnl.data.Exc;
 import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.index.Term;
 import org.apache.lucene.search.*;
 import org.apache.lucene.store.FSDirectory;
 import edu.stanford.nlp.simple.Sentence;
+import prep.Property;
 
 import java.io.IOException;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 /**
  * A collection of discriminativity classifiers based on dictionary models
  */
 public class DictionaryClassifiers {
+    public static int roleBasedVote(
+            String pivot, String comparison, String feature, String indexLocation) throws IOException{
+        BooleanQuery.Builder builderPivot = new BooleanQuery.Builder();
+        BooleanQuery.Builder builderComparison = new BooleanQuery.Builder();
+        builderPivot.add(new TermQuery(new Term("definiendum", pivot)), BooleanClause.Occur.MUST);
+        builderPivot.add(new WildcardQuery(new Term("*", feature)), BooleanClause.Occur.MUST);
+
+        builderComparison.add(new TermQuery(new Term("definiendum", comparison)), BooleanClause.Occur.MUST);
+        builderComparison.add(new WildcardQuery(new Term("*", feature)), BooleanClause.Occur.MUST);
+
+        BooleanQuery queryPivot = builderPivot.build();
+        BooleanQuery queryComparison = builderComparison.build();
+
+        DirectoryReader reader;
+        IndexSearcher searcher;
+        try {
+            reader = DirectoryReader.open(FSDirectory.open(Paths.get(indexLocation)));
+            searcher = new IndexSearcher(reader);
+        } catch (IOException e) {
+            e.printStackTrace();
+            throw new IllegalArgumentException("Invalid Index directory specified.");
+        }
+
+        ScoreDoc[] resultsComparison, resultsPivot;
+        try {
+            resultsComparison = searcher.search(queryComparison, 50).scoreDocs;
+            resultsPivot = searcher.search(queryPivot, 50).scoreDocs;
+        } catch (IOException e) {
+            throw new RuntimeException("Failed to obtain search results for Index query.");
+        }
+
+        // Lists containing only pivot/comparison properties with feature as their value
+        ArrayList<Property> pivotFeatureProperties = new ArrayList<>();
+        ArrayList<Property> comparisonFeatureProperties = new ArrayList<>();
+
+        for(ScoreDoc pivotDoc : resultsPivot){
+            for(IndexableField field : searcher.doc(pivotDoc.doc).getFields()){
+                if(field.stringValue().equals(feature)){
+                    pivotFeatureProperties.add(new Property(field.stringValue(), field.name()));
+                }
+            }
+        }
+
+        for(ScoreDoc comparisonDoc : resultsComparison){
+            for(IndexableField field : searcher.doc(comparisonDoc.doc).getFields()){
+                if(field.stringValue().equals(feature)){
+                    pivotFeatureProperties.add(new Property(field.stringValue(), field.name()));
+                }
+            }
+        }
+
+        pivotFeatureProperties.removeAll(comparisonFeatureProperties);
+        if(pivotFeatureProperties.size() > 1){
+            return 1;
+        }else {
+            return 0;
+        }
+
+    }
+
     public static int wordNetVote(String pivot, String comparison, String feature, String indexLocation) {
         pivot = new Sentence(pivot).lemma(0);
         comparison = new Sentence(comparison).lemma(0);
